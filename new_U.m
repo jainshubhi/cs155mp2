6c93180aa98dd79ce36036fa19cdc2a0750e37ef
Dylan Schultz <dylanschultz@beavernet-165.caltech.edu>
2015-03-11 22:35:08 -0700

    Added all files

---
+++ new_U.m
@@ -0,0 +1,35 @@
+function [ U ] = new_U( Y, V, lambda )
+%new_U Calculate U based on Y, V and lambda
+%   As part of the alternating least squares solution for solving for
+%   Y = UV', we hold V constant and calculate the optimal U.
+% Find the number of latent factors.
+[~, k] = size(V);
+% Get dimensions of Y.
+[m, n] = size(Y);
+% Initialize U.
+U = zeros(m, k);
+% Create a kxk identity.
+I = eye(k);
+% Scale identity by regulation factor lambda.
+L = lambda*I;
+% Find each row of U using the least squares optimal solution.
+for i = 1:m
+    % Calculate the V matrix which is a sum of matrices created by columns
+    % of V transpose.
+    % Calculate the y_vector which is a sum of vectors of v scaled by 
+    % y(i, j).
+    Vmat = zeros(k, k);
+    y_vec = zeros(k, 1);
+    for j = 1:n
+        % Only learn on entries of Y that exist.
+        if Y(i, j) ~= 0
+            Vmat = Vmat + V(j, :)'*V(j, :);
+            y_vec = y_vec + Y(i, j)*V(j,:)';
+        end
+    end
+    % Calculate the row of U
+    U(i, :) = ((L+Vmat)\y_vec)';
+end
+
+end
+
---
+++ new_V.m
@@ -0,0 +1,35 @@
+function [ V ] = new_V( Y, U, lambda )
+%new_V Calculate V based on Y, U and lambda
+%   As part of the alternating least squares solution for solving for
+%   Y = UV', we hold U constant and calculate the optimal V.
+% Find the number of latent factors.
+[~, k] = size(U);
+% Get dimensions of Y.
+[m, n] = size(Y);
+% Initialize V.
+V = zeros(n, k);
+% Create a kxk identity.
+I = eye(k);
+% Scale identity by regulation factor lambda.
+L = lambda*I;
+% Find each row of V using the least squares optimal solution.
+for j = 1:n
+    % Calculate the U matrix which is a sum of matrices created by rows
+    % of U.
+    % Calculate the y_vector which is a sum of vectors of u scaled by 
+    % y(i,j). 
+    Umat = zeros(k, k);
+    y_vec = zeros(k, 1);
+    for i = 1:m
+        % Only learn on entries of Y that exist.
+        if Y(i, j) ~= 0
+            Umat = Umat + U(i, :)'*U(i, :);
+            y_vec = y_vec + Y(i, j)*U(i,:)';
+        end
+    end
+    % Calculate the row of U
+    V(j, :) = ((L+Umat)\y_vec)';
+end
+
+end
+
